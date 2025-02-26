22e06121847f19b4d61f127e16dc09c161fd00ef
tycoon114 <sly114@naver.com>
2025-02-26 11:18:50 +0900

    슈팅게임 프로토 타입

--- Day24/Assets/Scripts/EnemyController.cs
+++ Day24/Assets/Scripts/EnemyController.cs
@@ -3,10 +3,37 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed = 5.0f;
+    Vector3 dir;
+    private void Start()
+    {
+        
+        int rand = Random.Range(0, 10);
+
+        if (rand < 3)
+        {
+            var target = GameObject.FindGameObjectWithTag("Player");
+
+            dir = target.transform.position - transform.position;
+
+            dir.Normalize(); //방향의 크기를 1로 설정
+        }
+        else {
+            dir = Vector3.down;
+        
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * speed * Time.deltaTime;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+
+        
     }
 }
--- Day24/Assets/Scripts/EnemyManager.cs
+++ Day24/Assets/Scripts/EnemyManager.cs
@@ -6,10 +6,25 @@ public class EnemyManager : MonoBehaviour
     public float createTime = 1.0f;
 
     public GameObject enemyFactory;
+    float min = 1, max = 5;
+
+    private void Start()
+    {
+        createTime = Random.Range(min, max);
+    }
 
     private void Update()
     {
-        
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= createTime) {
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = transform.position;
+
+            currentTime = 0;
+            createTime = Random.Range(min, max);
+        }
+
     }
 
 
