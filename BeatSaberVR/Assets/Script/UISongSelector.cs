8f043c295d9df9c3c34a79ceecd9f938e14bd915
tycoon114 <sly114@naver.com>
2025-06-30 12:14:46 +0900

    VR

---
+++ BeatSaberVR/Assets/Script/UISongSelector.cs
@@ -0,0 +1,73 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UISongSelector : MonoBehaviour
+{
+    [SerializeField]
+    class SongInfo
+    {
+        public string songTitle;
+        public GameObject songCard;
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < _songInfos.Length; i++)
+        {
+            _songInfos[i].songCard.SetActive(i==_selectedIndex);
+        }
+        _songTitle.text = _songInfos[_selectedIndex].songTitle; // 초기 제목 설정
+    }
+
+    public int selectedIndex
+    {
+        get => _selectedIndex;
+        set
+        {
+            _songInfos[_selectedIndex].songCard.SetActive(false); // 이전 카드 비활
+            _selectedIndex = value;
+            _songInfos[_selectedIndex].songCard.SetActive(true); // 새 카드 활성화
+            _songTitle.text = _songInfos[_selectedIndex].songTitle; // 제목 업데이트   
+        }
+    }
+
+    [SerializeField] TMP_Text _songTitle;
+    [SerializeField] SongInfo[] _songInfos;
+    [SerializeField] Button _next;
+    [SerializeField] Button _prev;
+    [SerializeField] Button _play;
+    int _selectedIndex;
+
+
+    private void OnEnable()
+    {
+        _next.onClick.AddListener(Next);
+        _prev.onClick.AddListener(Prev);
+        _play.onClick.AddListener(Play);
+    }
+
+    private void OnDisable()
+    {
+        _next.onClick.RemoveListener(Next);
+        _prev.onClick.RemoveListener(Prev);
+        _play.onClick.RemoveListener(Play);
+    }
+
+    public void Next()
+    {
+        selectedIndex = (_selectedIndex + 1) % _songInfos.Length;
+    }
+
+    public void Prev()
+    {
+        selectedIndex = (_selectedIndex - 1 + _songInfos.Length) % _songInfos.Length;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+}
